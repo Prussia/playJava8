c4a8f8e2387c0912883138042e4bc846a6f766d9
Prussia <prussia.hu@pwc.com>
2017-08-08 11:07:14 +0800

    Lambda

---
+++ src/main/java/org/prussia/play/java/eight/lambda/PlayLambda.java
@@ -0,0 +1,33 @@
+package org.prussia.play.java.eight.lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class PlayLambda {
+
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+
+		Collections.sort(names, new Comparator<String>() {
+		    @Override
+		    public int compare(String a, String b) {
+		    	System.out.println("a = " + a);
+		    	System.out.println("b = " + b);
+		    	int result = b.compareTo(a);
+		    	System.out.println(result);
+		        return result;
+		    }
+		});
+		
+		Collections.sort(names, (String a, String b) -> {
+			System.out.println("a = " + a);
+	    	System.out.println("b = " + b);
+			int result = b.compareTo(a);
+			System.out.println(result);
+		    return result;
+		});
+	}
+
+}
